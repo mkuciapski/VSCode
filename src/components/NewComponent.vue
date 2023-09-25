a55b8c2fc5c254374bd8056b0917a281dec97e35
mkuciapski <m.kuciapski@gmail.com>
2023-09-25 15:35:14 +0200

    Initial

---
+++ src/components/NewComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const myName = ref({ Name: 'MK' })
+
+myName.value.Name = 'DT'
+
+const languages = ref<string[]>([])
+
+onMounted(() => {
+  languages.value = ['C#', 'JS']
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="language in languages" :key="language">{{ language }}</li>
+  </ul>
+
+  <div>My new component: {{ myName.Name.toLocaleLowerCase() }}</div>
+
+  <div></div>
+</template>
+
+<style scoped></style>
